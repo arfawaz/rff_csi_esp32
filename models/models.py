b34497f03c8e00c49183f1618f4f18205fe2e588
fawaz <fawaz243@usf.edu>
2025-01-23 20:21:39 -0500

    Added a new folder Models with a new file models.py inside. Currently only one model is inside which is SimpleCNN

---
+++ models/models.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 23 20:16:46 2025
+
+@author: fawaz
+"""
+
+###############################################################################
+#Simple CNN model 
+
+'''
+This model is designed specifically to classify the data from Adam Plutos.This model has two Convolution Layers.
+Input data is of shape 64by2. This model is mostly hardcoded and
+only the number of classes (num_classes) can be varied.
+
+'''
+class SimpleCNN(nn.Module):
+    def __init__(self,num_classes):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(3, 1),padding=(1, 0))
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 1),padding=(1, 0))
+        self.fc1 = nn.Linear(4096, 128)  # Adjusted based on the output shape of conv layers
+        self.fc2 = nn.Linear(128, num_classes)  # 4 output classes
+        self.pool = nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1), padding=(0, 0))
+
+    def forward(self, x):
+        #the shape of the input is : 999by2
+        x = torch.relu(self.conv1(x)) #the shape after the first convolution lahyer is : 
+        x = torch.relu(self.conv2(x))
+        #x = self.pool(x)
+        x = x.view(x.size(0), -1)  # Flatten the tensor
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+###############################################################################
