e93e0255d582e4bf036e3ca75b7c5d3f28216bbd
fawaz <fawaz243@usf.edu>
2025-03-27 17:12:21 -0400

    Split the dataloading and training set into data loading for vit_model_2 and SimpleCNN. Added two other cells Training and testing on SimpleCNN and Training and testing on vit_model_2 for training and testing the respecitve models. Also added some imports. Modified the testing on new datset cell to use process_csv_fixed_id function to load data and labels.

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ from csi_dataset_creator import process_csv
 from mean_norm import mean_norm
 from train_test import train, test
 from train_test_loader import train_test_loader
-from models import SimpleCNN
+from train_vit_model_2 import train_vit_model_2
+from test_vit_model_2 import test_vit_model_2
+from CustomDataset_vit_model_2 import CustomDataset_vit_model_2
+from models import SimpleCNN, vit_model_2
+from torch.utils.data import Subset, DataLoader, TensorDataset, random_split
 import torch.nn as nn
 import torch
 import torch.optim as optim
@@ -20,7 +24,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
 
-#%% Training and Testing 
+#%% Loading data and labels for CNN and vit_model_2
 
 # Prompt the user for the file path
 file_path = input("Please enter the file path to the CSV file: ")
@@ -30,7 +34,14 @@ data, labels = process_csv_fixed_id_uniform_sampling(file_path = file_path , mac
 ["00:FC:BA:38:4B:00", \
 "00:FC:BA:38:4B:01", \
 "00:FC:BA:38:4B:02", \
-"2A:C8:A7:E1:8F:F0"], max_samples_per_mac=1000)
+"70:DB:98:9E:3A:A0", \
+"70:DB:98:9E:3A:A1"], max_samples_per_mac=1000)
+    
+print("Done data loading")
+    
+###############################################################################
+    
+#%% Training and testing on SimpleCNN
 data = data.unsqueeze(1)
 dataset = mean_norm(data)
 train_loader, test_loader = train_test_loader(dataset, labels)
@@ -54,10 +65,45 @@ model.eval()
 _ = test(model, test_loader)
 
 
+#%% Training and testing on vit_model_2
+dataset_vit_model_2 = CustomDataset_vit_model_2(data, labels)
+batch_size = 16
+# Split into train and test datasets
+train_size = int(0.9 * len(dataset_vit_model_2))
+test_size = len(dataset_vit_model_2) - train_size
+train_dataset_vit_model_2, test_dataset_vit_model_2 = random_split(dataset_vit_model_2, [train_size, test_size])
+train_loader_vit_model_2 = DataLoader(train_dataset_vit_model_2, batch_size=batch_size, shuffle=True)
+test_loader_vit_model_2 = DataLoader(test_dataset_vit_model_2, batch_size=batch_size, shuffle=False)
+
+num_classes = 5
+learning_rate = 5e-5
+num_epochs = 50
+
+model = vit_model_2(num_classes = num_classes)
+model.to(device)
+
+# Set up the optimizer and loss function
+optimizer = AdamW(model.parameters(), lr=learning_rate)
+loss_fn = torch.nn.CrossEntropyLoss()
+
+
+# Training the model
+for epoch in range(num_epochs):
+    print(f"Epoch {epoch + 1}/{num_epochs}")
+
+    # Training
+    train_loss, train_accuracy = train_vit_model_2(model = model, train_loader = train_loader_vit_model_2, optimizer = optimizer, loss_fn = loss_fn , device = device)
+    print(f"Training Loss: {train_loss:.4f}, Training Accuracy: {train_accuracy:.2f}%")
+
+    # Testing
+    test_loss, test_accuracy = test_vit_model_2(model = model, test_loader = test_loader_vit_model_2, loss_fn = loss_fn, device = device)
+    print(f"Testing Loss: {test_loss:.4f}, Testing Accuracy: {test_accuracy:.2f}%")
+
+
 #%% Testing on new test dataset
 
 file_path = input("Please enter the file path to the test CSV file: ")
-data, labels = process_csv(file_path = file_path , mac_id_list = ["34:5F:45:A8:3C:19", \
+data, labels =  process_csv_fixed_id_uniform_sampling(file_path = file_path , mac_id_list = ["34:5F:45:A8:3C:19", \
 "8C:4F:00:3C:BF:4D", \
 "3C:8A:1F:90:E3:31", \
 "34:5F:45:A9:A4:19", \
