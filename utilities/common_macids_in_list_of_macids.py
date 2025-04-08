bda1610d5ad0ab3e7bca6fd0c484e15381190c5c
fawaz <fawaz243@usf.edu>
2025-04-08 16:20:31 -0400

    Added the file utilities/common_macids_in_list_of_macids.py which contains the function common_macids_in_list_of_macids() which accepts a list of list of macids and then prints out the list of common macids in all the lists of macids and also returns the list of common macids.

---
+++ utilities/common_macids_in_list_of_macids.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  8 16:14:23 2025
+
+@author: fawaz
+"""
+
+def common_macids_in_list_of_macids(mac_lists):
+    """
+    Takes a list of lists containing MAC IDs and returns a list of MAC IDs
+    that are common across all sublists.
+
+    Parameters:
+    mac_lists (list of list of str): A list containing multiple lists of MAC ID strings.
+
+    Returns:
+    list of str: MAC IDs that appear in every list.
+    """
+    if not mac_lists:
+        return []
+
+    # Start with the set of MACs from the first list
+    common_macids = set(mac_lists[0])
+
+    # Intersect with MACs from each subsequent list
+    for mac_list in mac_lists[1:]:
+        common_macids.intersection_update(mac_list)
+
+    # Return as a list (sorted optionally)
+    print("Common MAC IDs accross the list of MAC IDs: {common_macids}")
+    return list(common_macids)
