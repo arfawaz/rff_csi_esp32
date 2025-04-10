c818449144f67015bab5ea9614a1a1e07af44c43
fawaz <fawaz243@usf.edu>
2025-04-10 17:29:43 -0400

    Added build_macid_position_table() funtion from build_macid_table.py file to test/function_usage.py. This function is used to count the number of samples for each positon (passed as a .csv file for each position) and display a table. It also display a table with scaled verion of the table accoring the least count of macids.

--- test/function_usage.py
+++ test/function_usage.py
@@ -189,20 +189,46 @@ combine_csv_files(file_paths=file_paths, output_file=output_file)
 #%% combine_csv_files_4_cols()
 
 file_paths = [
-    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\10_26_feb_25_p4_05_00_07_00.csv", \
-    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\12_27_feb_25_p4_11_50_01_05.csv", \
-    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\2_24_feb_25_p4_05_17_06_55.csv", \
-    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\38_08_mar_25_p4_06_20_07_20.csv", \
-    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\42_09_mar_25_p4_01_50_02_20.csv", \
-    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\6_25_feb_25_p4_03_05_05_00.csv", \
-    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\73_24_mar_25_p4_10_00_12_00.csv", \
-    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\74_25_mar_25_p4_04_00_07_00.csv", \
-    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\88_03_apr_25_p4_07_55_10_55.csv", \
-    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\89_05_apr_25_p4_02_30_05_30.csv", \
-    #r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\92_06_apr_25_p4_02_05_05_05.csv" \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\13_27_feb_25_p5_01_10_01_45.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\10_26_feb_25_p4_05_00_07_00.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\15_27_feb_25_p6_03_15_05_00.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\11_26_feb_25_p5_07_00_.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\12_27_feb_25_p4_11_50_01_05.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\14_27_feb_25_p5_02_50_.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\21_01_mar_25_p6_12_35_01_15.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\2_24_feb_25_p4_05_17_06_55.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\38_08_mar_25_p4_06_20_07_20.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\39_08_mar_25_p5_07_20_08_25.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\3_24_feb_25_p5_07_00_09_00.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\40_08_mar_25_p6_08_25_09_25.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\42_09_mar_25_p4_01_50_02_20.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\43_09_mar_25_p5_02_20_02_50.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\44_09_mar_25_p6_02_50_03_20.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\47_10_mar_25_p5_05_45_06_45.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\48_10_mar_25_p6_06_45_07_20.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\4_25_feb_25_p6_1_00_01_15.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\50_11_mar_25_p6_06_45_08_10.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\68_20_mar_25_p6_03_00_05_00.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\6_25_feb_25_p4_03_05_05_00.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\73_24_mar_25_p4_10_00_12_00.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\74_25_mar_25_p4_04_00_07_00.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\79_31_mar_25_p5_04_50_08_10.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\7_25_feb_25_p5_05_05_07_00.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\80_31_mar_25_p6_08_10_10_55.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\86_03_apr_25_p5_01_00_04_20.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\87_03_apr_25_p6_04_45_07_50.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\88_03_apr_25_p4_07_55_10_55.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\89_05_apr_25_p4_02_30_05_30.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\8_25_feb_25_p6_07_05_09_00.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\90_05_apr_25_p5_05_30_8_40.csv", \
+    # r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\91_05_apr_25_p6_08_40_11_40.csv", \
+     r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\92_06_apr_25_p4_02_05_05_05.csv", \
+     r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\93_06_apr_25_p5_05_05_08_05.csv", \
+     r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\94_07_apr_25_p6_04_20_07_20.csv"
 ]
 
-output_file =  r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\all_but_last_merged\p4_all_but_last_merged.csv" 
+
+output_file =  r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6\p4_p5_p6_last_merged\p4_p5_p6_last_merged.csv"
 combine_csv_files_4_cols(file_paths=file_paths, output_file=output_file)
 
 #%% count_mac_occurrences
@@ -214,7 +240,7 @@ count_mac_occurrences(file_path, n=n)
 
 #%% get_n_csv_filepaths()
 
-folder_path = r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\systematic_collection_numbered\systematic_collection_numbered"
+folder_path = r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4_p5_p6"
 n = 1000  # Number of files you want to retrieve
 
 get_n_csv_filepaths(folder_path, n)
@@ -225,7 +251,7 @@ source_folder = '/home/fawaz/Desktop/USF/PHD/COURSES/SPRING25/projects_on_git/rf
 destination_folder = '/home/fawaz/Desktop/USF/PHD/COURSES/SPRING25/projects_on_git/rff_csi_esp32/data_collection/systematic_collection_numbered' 
 copy_csv_with_serial(source_folder, destination_folder)
 
-#%% count_macid_occurrences
+#%% count_target_macid_occurrences
 
 target_macids = [
 "6C:B2:AE:39:1A:A0", \
@@ -242,9 +268,30 @@ target_macids = [
 "84:3D:C6:5F:5D:50"
 ]  # List of MAC IDs to track
     
-csv_file = r'C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\p4_last.csv'  # Replace with the path to your CSV file
+csv_file = r'C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\all_but_last_merged\p4_all_but_last_merged.csv'  # Replace with the path to your CSV file
 
 # Run the function to count occurrences
-count_macid_occurrences(csv_file, target_macids)
+count_target_macid_occurrences(csv_file, target_macids)
 
+#%% build_macid_position_table
 
+mac_ids = [
+"6C:B2:AE:39:1A:A0", \
+"70:0F:6A:DE:EC:A0", \
+"70:0F:6A:DE:EC:A1", \
+"6C:B2:AE:39:1A:A1", \
+"70:0F:6A:DE:EC:A2", \
+"6C:B2:AE:39:1A:A2", \
+"C8:28:E5:44:3B:00", \
+"00:FC:BA:38:4B:00", \
+"00:FC:BA:38:4B:01", \
+"70:0F:6A:FC:51:80", \
+"00:FC:BA:38:4B:02", \
+"84:3D:C6:5F:5D:50"
+]
+file_paths = [
+    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p4\all_but_last_merged\p4_all_but_last_merged.csv",
+    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p5\all_but_last_merged\p5_all_but_last_merged.csv",
+    r"C:\Users\fawaz\OneDrive - University of South Florida\Desktop\USF\SEMESTER 1 - FALL 23\DIRECTED RESEARCH\projects_on_git\rff_csi_esp32\csi_data_collected\Individual_positions\p6\all_but_last_merged\p6_all_but_last_merged.csv"
+]
+count_dict, adjusted_counts = build_macid_position_table(mac_ids, file_paths)
