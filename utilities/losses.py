483cad3c38fedc1e570b97ad85e32d84bf4701f5
fawaz <fawaz243@usf.edu>
2025-09-13 16:00:36 -0400

    Added the losses.py file in utilities for the loss function. The first loss function is the clip_loss which calculates the CSI to Label and Label to CSI classification loss and finally both added together and averaged.

---
+++ utilities/losses.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 13 15:47:17 2025
+
+@author: fawaz243
+"""
+
+# clip loss
+def clip_loss(logits):
+    
+    B = logits.size(0)
+    target = torch.arange(B, device = logits.device)
+    loss_i = F.cross_entropy(logits, target)
+    loss_t = F.cross_entropy(logits.t(), target)
+    return 0.5 * (loss_i + loss_t)
